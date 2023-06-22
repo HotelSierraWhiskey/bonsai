3d33486616eda2779418ce40a649bf516aac6e80
HotelSierraWhiskey <winstontothemoon@gmail.com>
2023-06-21 21:02:08 -0400

    added del, mov, put, get

--- bonsai.hpp
+++ bonsai.hpp
@@ -15,15 +15,32 @@ class Bonsai {
     system_properties_row_t system;
     Nvmc nvm;
     part_t part;
-    void update_free_space_address(uint32_t address) noexcept;
 
     Bonsai(void);
-    part_t get_part(void) noexcept;
-    void erase(void) noexcept;
-    uint32_t read_free_space_address(void) noexcept;
-    void fputf(file_t &file) noexcept;
-    file_t fgetf(uint32_t address);
-    void create_file(std::string path) noexcept;
+    part_t get_part(void);
+
+    /* Erases entire Bonsai flash partition */
+    void erase(void);
+
+    /* Read FSA from flash */
+    uint32_t read_free_space_address(void);
+
+    /* Writes new FSA to flash */
+    void update_free_space_address(const uint32_t address);
+
+    /* Write a file to flash (updates FSA) */
+    void put(file_t &file);
+    void put(file_t &file, uint32_t address);
+    void put_blank_file(const std::string name, uint32_t parent_address, uint32_t address);
+
+    /* Return file_t from address */
+    file_t get(const uint32_t address);
+
+    /* deletes the file at address */
+    void del(uint32_t address);
+
+    /* moves file data from flash address src to dest */
+    void mov(const uint32_t dest, const uint32_t src);
 };
 
 #endif
