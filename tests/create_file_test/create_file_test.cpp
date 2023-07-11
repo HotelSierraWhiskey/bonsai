48d4720d2e5eb43e7084c0c698bbb9af4965c043
HotelSierraWhiskey <winstontothemoon@gmail.com>
2023-07-11 15:49:19 -0400

    added test

---
+++ tests/create_file_test/create_file_test.cpp
@@ -0,0 +1,9 @@
+#include "create_file_test.hpp"
+
+
+void create_file_test(void) {
+    Bonsai bonsai;
+    bonsai.create_file("root/file1");
+    // bonsai.create_file("root/file1/file1_child2");
+    // bonsai.create_file("root/file1/file1_child3");
+}
---
+++ tests/create_file_test/create_file_test.hpp
@@ -0,0 +1,10 @@
+#ifndef CREATE_FILE_TEST_HPP
+#define CREATE_FILE_TEST_HPP
+
+#include "bonsai/tests/test.hpp"
+
+
+extern void create_file_test(void);
+
+
+#endif
