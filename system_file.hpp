337e7ca9533f6dd59a9cd9c7547e18d4d248eeea
HotelSierraWhiskey <winstontothemoon@gmail.com>
2023-07-18 14:07:02 -0400

    changed to LSB

--- system_file.hpp
+++ system_file.hpp
@@ -6,7 +6,7 @@
 typedef struct {
     uint8_t _fsa[4];
     uint32_t fsa(void) {
-        return (_fsa[0] << 24) | (_fsa[1] << 16) | (_fsa[2] << 8) | (_fsa[3]); //
+        return (_fsa[3] << 24) | (_fsa[2] << 16) | (_fsa[1] << 8) | (_fsa[0]); //
     }
 } system_file_data_t;
 
