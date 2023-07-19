e37f7c5a8a931846cf063e17dea28559f19083f5
HotelSierraWhiskey <winstontothemoon@gmail.com>
2023-07-19 11:45:35 -0400

    fixed delete_file()

--- src/api.cpp
+++ src/api.cpp
@@ -33,21 +33,20 @@ void Bonsai::delete_file(std::string path) {
     }
 
     size_t pos = 0;
-    uint32_t current_addr = ROOT_DIRECTORY_ADDRESS;
-    uint32_t prev_addr = current_addr;
     std::string handle;
+    auto saved_fsa = fsa;
 
-    uint32_t found_addr = U32_FLASH_RESET_VALUE;
-
-    const auto saved = fsa;
     while ((pos = path.find("/")) != std::string::npos) {
         handle = path.substr(0, pos);
-        found_addr = find(current_addr, handle);
-        prev_addr = current_addr;
-        current_addr = found_addr;
         path.erase(0, pos + 1);
     }
-    found_addr = find(current_addr, handle);
-    del(found_addr);
-    remove_child_addr(prev_addr, found_addr);
+
+    auto addr = find(ROOT_DIRECTORY_ADDRESS, handle);
+
+    if (addr != U32_FLASH_RESET_VALUE) {
+        auto file = get(addr);
+        remove_child_addr(file.parent_addr, addr);
+        del(addr);
+        fsa = saved_fsa;
+    }
 }
