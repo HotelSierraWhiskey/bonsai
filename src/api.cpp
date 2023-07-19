3d706db1658b2b9717012da84009d7aa84638097
HotelSierraWhiskey <winstontothemoon@gmail.com>
2023-07-19 13:38:13 -0400

    implemented delete_subfiles()

--- src/api.cpp
+++ src/api.cpp
@@ -46,7 +46,27 @@ void Bonsai::delete_file(std::string path) {
     if (addr != U32_FLASH_RESET_VALUE) {
         auto file = get(addr);
         remove_child_addr(file.parent_addr, addr);
-        del(addr);
+        delete_subfiles(addr);
         fsa = saved_fsa;
     }
 }
+
+void Bonsai::delete_subfiles(uint32_t root_addr) {
+        uint32_t address_stack[32];
+        size_t index = 0;
+
+        address_stack[index++] = root_addr;
+
+        while (index) {
+            uint32_t addr = address_stack[--index];
+
+            auto file = get(addr);
+
+            if (file.num_child_addrs) {
+                for (uint8_t i = 0; i < file.num_child_addrs; i++) {
+                    address_stack[index++] = file.child_addrs[i];
+                }
+            }
+            del(addr);
+        }
+    }
