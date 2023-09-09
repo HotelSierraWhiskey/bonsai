356e1439dc517f96477311755cb5a9063afa1149
HotelSierraWhiskey <winstontothemoon@gmail.com>
2023-09-09 14:21:04 -0400

    fixed delete_file()

--- src/api.cpp
+++ src/api.cpp
@@ -32,41 +32,40 @@ void Bonsai::delete_file(std::string path) {
         path += '/';
     }
 
-    size_t pos = 0;
-    std::string handle;
     auto saved_fsa = fsa;
 
-    while ((pos = path.find("/")) != std::string::npos) {
-        handle = path.substr(0, pos);
-        path.erase(0, pos + 1);
-    }
-
-    auto addr = find(ROOT_DIRECTORY_ADDRESS, handle);
+    auto addr = find(path);
 
     if (addr != U32_FLASH_RESET_VALUE) {
         auto file = get(addr);
         remove_child_addr(file.parent_addr, addr);
-        delete_subfiles(addr);
+        delete_recursive(addr);
         fsa = saved_fsa;
     }
 }
 
-void Bonsai::delete_subfiles(uint32_t root_addr) {
-        uint32_t address_stack[32];
-        size_t index = 0;
+void Bonsai::delete_recursive(uint32_t root_addr) {
+    uint32_t address_stack[32];
+    size_t index = 0;
 
-        address_stack[index++] = root_addr;
+    address_stack[index++] = root_addr;
 
-        while (index) {
-            uint32_t addr = address_stack[--index];
+    while (index) {
+        uint32_t addr = address_stack[--index];
 
-            auto file = get(addr);
+        auto file = get(addr);
 
-            if (file.num_child_addrs) {
-                for (uint8_t i = 0; i < file.num_child_addrs; i++) {
-                    address_stack[index++] = file.child_addrs[i];
-                }
+        if (file.num_child_addrs) {
+            for (uint8_t i = 0; i < file.num_child_addrs; i++) {
+                address_stack[index++] = file.child_addrs[i];
             }
-            del(addr);
         }
+        del(addr);
     }
+}
+
+void Bonsai::move_file(std::string dest, std::string src) {}
+
+void Bonsai::read_file(std::string path) {}
+
+void Bonsai::write_to_file(std::string path, std::string data) {}
